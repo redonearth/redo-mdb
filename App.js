ee91003800bf4367f06fabc5d0c263b019dde8a7
Redonearth <wona23@gmail.com>
2022-09-26 21:05:24 +0900

    Fonts, Assets 준비 로직 작성

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+import { Asset } from 'expo-asset';
+import { Image, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+SplashScreen.preventAutoHideAsync();
+
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+const loadAssets = (assets) =>
+  assets.map((asset) => {
+    if (typeof asset === 'string') {
+      return Image.prefetch(asset);
+    } else {
+      return Asset.loadAsync(asset);
+    }
+  });
 
 export default function App() {
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    async function prefare() {
+      try {
+        const fonts = loadFonts([Ionicons.font]);
+        const assets = loadAssets([require('./redonearth.png')]);
+        await Promise.all([...fonts, ...assets]);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setReady(true);
+      }
+    }
+
+    prefare();
+  }, []);
+
+  const onLayout = useCallback(async () => {
+    if (ready) {
+      await SplashScreen.hideAsync();
+    }
+  }, [ready]);
+
+  if (!ready) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View onLayout={onLayout}>
+      <Text>Hahaha</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
