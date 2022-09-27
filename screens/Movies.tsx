e3eeb7d419a669da3a831e912badfee38cdd194a
Redonearth <wona23@gmail.com>
2022-09-28 05:03:03 +0900

    Trending Movies 로직 작성

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components/native';
-import { ActivityIndicator, Dimensions } from 'react-native';
+import {
+  ActivityIndicator,
+  Dimensions,
+  ScrollView,
+  useColorScheme,
+} from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import Swiper from 'react-native-swiper';
 import Slide from '../components/Slide';
-
-const API_KEY = '5efe6813c047c5a28d580b92e30a6262';
-
-const URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`;
+import Poster from '../components/Poster';
 
 const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
@@ -22,17 +24,81 @@ const Loader = styled.View`
 
 const { height: SCREEN_HEIGHT } = Dimensions.get('window');
 
+const ListTitle = styled.Text<{ isDark: boolean }>`
+  color: ${(props) => (props.isDark ? 'white' : 'black')};
+  font-size: 20px;
+  font-weight: 600;
+  margin-left: 30px;
+`;
+
+const TrendingScroll = styled.ScrollView`
+  margin-top: 15px;
+`;
+
+const Movie = styled.View`
+  margin-right: 15px;
+  align-items: center;
+`;
+
+const Title = styled.Text<{ isDark: boolean }>`
+  color: ${(props) => (props.isDark ? 'white' : 'black')};
+  font-weight: 600;
+  margin-top: 8px;
+  margin-bottom: 2px;
+`;
+
+const Votes = styled.Text<{ isDark: boolean }>`
+  color: ${(props) =>
+    props.isDark ? 'rgba(255, 255, 255, 0.75)' : 'rgba(0, 0, 0, 0.75)'};
+  font-size: 10px;
+  font-weight: 500;
+`;
+
+const BASE_URL = `https://api.themoviedb.org/3`;
+const API_KEY = '5efe6813c047c5a28d580b92e30a6262';
+const LANGUAGE = `language=en-US`;
+const PAGE = `page=1`;
+const REGION = `region=KR`;
+const requestUrl = (url: string, query: string = '') =>
+  `${BASE_URL}${url}?api_key=${API_KEY}${query}`;
+
 export default function Movies({}: NativeStackScreenProps<any, 'Movies'>) {
+  const isDark = useColorScheme() === 'dark';
   const [loading, setLoading] = useState(true);
   const [nowPlaying, setNowPlaying] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
+  const [trending, setTrending] = useState([]);
   const getNowPlaying = async () => {
-    const { results } = await (await fetch(URL)).json();
+    const { results } = await (
+      await fetch(
+        requestUrl(`/movie/now_playing`, `&${LANGUAGE}&${PAGE}&${REGION}`)
+      )
+    ).json();
     setNowPlaying(results);
+  };
+  const getUpcoming = async () => {
+    const { results } = await (
+      await fetch(
+        requestUrl(`/movie/upcoming`, `&${LANGUAGE}&${PAGE}&${REGION}`)
+      )
+    ).json();
+    setUpcoming(results);
+  };
+  const getTrending = async () => {
+    const { results } = await (
+      await fetch(requestUrl(`/trending/movie/week`))
+    ).json();
+    console.log(requestUrl(`/trending/movie/week`));
+    setTrending(results);
+  };
+  const getData = async () => {
+    await Promise.all([getNowPlaying(), getUpcoming(), getTrending()]);
     setLoading(false);
   };
   useEffect(() => {
-    getNowPlaying();
+    getData();
   }, []);
+
   return loading ? (
     <Loader>
       <ActivityIndicator />
@@ -44,7 +110,11 @@ export default function Movies({}: NativeStackScreenProps<any, 'Movies'>) {
         autoplay
         autoplayTimeout={3.5}
         showsPagination={false}
-        containerStyle={{ width: '100%', height: SCREEN_HEIGHT / 4 }}
+        containerStyle={{
+          width: '100%',
+          height: SCREEN_HEIGHT / 4,
+          marginBottom: 30,
+        }}
       >
         {nowPlaying.map((movie) => (
           <Slide
@@ -57,6 +127,23 @@ export default function Movies({}: NativeStackScreenProps<any, 'Movies'>) {
           />
         ))}
       </Swiper>
+      <ListTitle isDark={isDark}>Trending Movies</ListTitle>
+      <TrendingScroll
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingLeft: 30 }}
+      >
+        {trending.map((movie) => (
+          <Movie key={movie.id}>
+            <Poster path={movie.poster_path} />
+            <Title isDark={isDark}>
+              {movie.original_title.slice(0, 13)}
+              {movie.original_title.length > 13 && '...'}
+            </Title>
+            <Votes isDark={isDark}>★ {movie.vote_average}/10</Votes>
+          </Movie>
+        ))}
+      </TrendingScroll>
     </Container>
   );
 }
