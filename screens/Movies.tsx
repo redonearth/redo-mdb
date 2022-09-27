cec45359e458c4c04b543fc399ad7a124debfcc0
Redonearth <wona23@gmail.com>
2022-09-27 16:44:56 +0900

    TypeScript 설정

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,7 +1,8 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
 import styled from 'styled-components/native';
 
-const Btn = styled.View`
+const Btn = styled.TouchableOpacity`
   flex: 1;
   justify-content: center;
   align-items: center;
@@ -12,7 +13,9 @@ const Title = styled.Text`
   color: ${(props) => props.theme.textColor};
 `;
 
-const Movies = ({ navigation: { navigate } }) => (
+const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = ({
+  navigation: { navigate },
+}) => (
   <Btn onPress={() => navigate('Stack', { screen: 'Three' })}>
     <Title>Movies</Title>
   </Btn>
