5554c3733afcc473621c43c7f39270289106f576
Redonearth <wona23@gmail.com>
2022-09-29 17:16:16 +0900

    js 👉 tsx 변경

--- navigation/Stack.tsx
+++ navigation/Stack.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 
-const ScreenOne = ({ navigation: { navigate } }) => (
+const ScreenOne = ({ navigation: { navigate } }: any) => (
   <TouchableOpacity onPress={() => navigate('Two')}>
     <Text>Go to Two</Text>
   </TouchableOpacity>
 );
-const ScreenTwo = ({ navigation: { navigate } }) => (
+const ScreenTwo = ({ navigation: { navigate } }: any) => (
   <TouchableOpacity onPress={() => navigate('Three')}>
     <Text>Go to Three</Text>
   </TouchableOpacity>
 );
-const ScreenThree = ({ navigation: { navigate } }) => (
+const ScreenThree = ({ navigation: { navigate } }: any) => (
   <TouchableOpacity onPress={() => navigate('Tabs', { screen: 'Search' })}>
     <Text>Go to Search</Text>
   </TouchableOpacity>
